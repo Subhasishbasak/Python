4df6f9528ffe78682e4e34ecb261b25b3a528341
Subhasishbasak <46485998+Subhasishbasak@users.noreply.github.com>
2019-05-07 17:45:54 +0530

    Add files via upload

---
+++ Miscellaneous/maps.py
@@ -0,0 +1,7 @@
+l = [2,100,29,97,33,38,59,67]
+
+def sqr(n):
+   return n*n
+
+
+print(list(map(sqr,l)))
---
+++ Miscellaneous/split.py
@@ -0,0 +1,12 @@
+def splitwith(s,c):
+  ans = []
+  curr = ""
+  for i in s:
+    if i == c:
+      ans.append(curr)
+      curr = ""
+    else:
+      curr = curr + i
+  ans.append(curr)
+  return(ans)
+
